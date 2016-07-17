9522d4e4c1df573bbe0260cd515786a45987735e
david <davidpark1208@gmail.com>
2016-07-17 23:26:47 +0900

    url, view 코딩

--- mysite/bookmark/views.py
+++ mysite/bookmark/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+from django.conf.urls import url
+from django.contrib import admin
+
+from django.views.generic import ListView, DetailView #generic view 사용
+from bookmark.models import Bookmark
 
 # Create your views here.
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
