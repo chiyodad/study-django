d8670a5031a19ae71df63d5aa509955e91e84f41
david <davidpark1208@gmail.com>
2016-07-17 23:00:24 +0900

    static dir ) 추가

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'] # 추가
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # 추가
 
 # Media files
 MEDIA_URL = '/media/'
