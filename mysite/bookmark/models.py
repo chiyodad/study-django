c45e6754a5aca00d7dffafc04bcb05549eecbbc0
david <davidpark1208@gmail.com>
2016-07-17 23:17:05 +0900

    model 생성 완료

--- mysite/bookmark/models.py
+++ mysite/bookmark/models.py
@@ -1,3 +1,15 @@
+from __future__ import unicode_literals # Python 2 지원
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
+@python_2_unicode_compatible
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    url = models.URLField('url', unique=True)
+
+    class Meta:
+        verbose_name = '북마크'
+
+    def __str__(self):
+        return self.title
